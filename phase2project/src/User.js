d7de01d9dd60af0ea7a5cdf0aa869def1cd0ed2f
Avi Kaul <avikarkaul@gmail.com>
2023-06-26 17:08:59 -0400

    got user, form to render. needs way more work

--- phase2project/src/User.js
+++ phase2project/src/User.js
@@ -1,17 +1,18 @@
 function User(){
     return (
-        <div className="user" style={{}}>
+        <div className="user-card">
             <img src={"profile.jpg"} alt={"name"} className="user-image" />
-            <p className="user-name" style={{backgroundColor:"red",borderRadius:"5px",padding:"1rem"}}>Avi Kaul</p>
-            <p className='user-description'>Coding wiz!</p>
+            <div className="user-info" style={{borderRadius:"5px", padding: "1rem"}}>
+                <p className="user-name" style={{backgroundColor:"yellow",borderRadius:"5px",padding:"1rem"}}>Avi Kaul</p>
+                <p className='user-number'>Number</p>
+                <p className='user-address'>Address</p>
+            </div>
             <button>Edit Contact</button>
             <div className="likes">
                 <img src={"logo512.png"} alt="like" style={{width:"32px",height:"32px",verticalAlign:"middle"}} />
                 <span style={{fontSize:"24px"}}>likes</span>
             </div>
         </div>
-
-
     )
 }
 export default User;
