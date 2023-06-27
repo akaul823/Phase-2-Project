38f9849585cf5bae530d5a64030ea9b94b98a8b6
Avi Kaul <avikarkaul@gmail.com>
2023-06-27 16:32:46 -0400

    data called

--- phase2project/src/App.js
+++ phase2project/src/App.js
@@ -2,15 +2,20 @@ import Header from './Header';
 import AccountNav from './AccountNav';
 import Messenger from './Messenger';
 import Contacts from './Contacts';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
   const [data, setData] = useState([])
+  useEffect(()=>{
+    fetch("http://localhost:4000/data")
+    .then(res=>res.json())
+    .then(data=>setData(data))
+  }, [])
   return (
     <div className="App">
       <Header />
-      <AccountNav />
+      <AccountNav data={data} />
       <Contacts />
       <Messenger />
     </div>
